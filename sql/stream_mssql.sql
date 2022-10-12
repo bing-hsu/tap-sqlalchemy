d776ad86d44742c7b9b9ae3e6d14618b2369a743
Bertram (Bingxu) Gao <bingxug@gmail.com>
2022-10-12 13:37:28 +0800

    remove local conf

--- sql/stream_mssql.sql
+++
@@ -1,1 +0,0 @@
-select * from (values (1, 'a', 'mssql'), (2, 'b', 'mssql'), (3, 'c', 'mssql')) T(a, b, c)
--- sql/stream_sqlite.sql
+++
@@ -1,3 +0,0 @@
-with T(a, b, c) as (values (1, 'a', 'sqlite'), (2, 'b', 'sqlite'), (3, 'c', 'sqlite'))
-select *
-from T
